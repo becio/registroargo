bb74ee17fd34e699e72aa57280e8bc705c4bdb33
spartaco.giubbolini <spartaco.giubbolini@mermecgroup.com>
2020-04-16 20:52:07 +0200

    Il download dei file ora ritorna anche il content-type

---
+++ src/School.Core/Models/DownloadResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Net.Http.Headers;
+
+namespace School.Core.Models
+{
+    public class DownloadResponse
+    {
+        public MediaTypeHeaderValue ContentType { get; set; }
+
+        public long? ContentLength { get; set; }
+
+        public Stream ResponseStream { get; set; }
+    }
+}
--- src/School.Core/Models/DownloadableFile.cs
+++ src/School.Core/Models/DownloadableFile.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -23,7 +22,7 @@ namespace School.Core.Models
 
         public string Name { get; }
 
-        public async Task<Stream> DownloadAsync()
+        public async Task<DownloadResponse> DownloadAsync()
         {
             var content = new FormUrlEncodedContent(new[]{
                 new KeyValuePair<string, string>("BackbaseClientDelta", $"[evt=panel-messaggiBacheca:pannello|event|custom|riga|{_code}|operazione|download]"),
@@ -41,7 +40,12 @@ namespace School.Core.Models
             ret = await _httpClient.PostAsync(generatorUrl, new StringContent(""));
             ret.EnsureSuccessStatusCode();
 
-            return await ret.Content.ReadAsStreamAsync();
+            return new DownloadResponse
+            {
+                ResponseStream = await ret.Content.ReadAsStreamAsync(),
+                ContentType = ret.Content.Headers.ContentType,
+                ContentLength = ret.Content.Headers.ContentLength
+            };
         }
     }
 }
